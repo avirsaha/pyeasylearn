61ac07268050aa8789bc068fca62f20ca85c03c9
Aviraj <aviraj.saha@outlook.com>
2023-06-27 16:09:40 +0530

    Initial commit.

---
+++ pytextflow/nlg.py
@@ -0,0 +1,6 @@
+import reframing
+import summarization
+
+__VERSION__ = "0.0.1-beta"
+
+__all__ = [reframing, summarization]
---
+++ pytextflow/nlp.py
@@ -0,0 +1,8 @@
+import preprocessing
+import statistics
+import pattern_matching
+import classification
+
+__VERSION__ = "0.0.1-alpha"
+
+__all__ = [preprocessing, statistics, pattern_matching, classification]
